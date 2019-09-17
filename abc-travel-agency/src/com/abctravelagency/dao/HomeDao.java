9d39026a3399359c84aea2300595d4999f295e72
Mohammad Helal Khan <m.helal.k@gmail.com>
2019-09-17 23:30:03 +0600

    Update HomeDao.java

--- abc-travel-agency/src/com/abctravelagency/dao/HomeDao.java
+++ abc-travel-agency/src/com/abctravelagency/dao/HomeDao.java
@@ -1,14 +1,12 @@
 package com.abctravelagency.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -29,21 +27,6 @@ public class HomeDao {
 	}
 	
 	public List<Status> getAllStatus() {
-		return jdbc.query("SELECT u.FirstName, u.LastName, Status, l.name, access, s.CreatedDate  FROM status s join user u on s.UserId = u.UserId join location l on s.LocationId = l.LocationId where access=0", new RowMapper<Status>() {
-			public Status mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Status status = new Status();
-				status.setStatus(rs.getString("Status"));
-				status.setFirstName(rs.getString("FirstName"));
-				status.setLastName(rs.getString("LastName"));
-				status.setLocation(rs.getString("Name"));
-				status.setAccess(rs.getInt("Access"));
-				status.setCreatedDate(rs.getTimestamp("CreatedDate"));
-				return status;
-			}
-		});
+		return jdbc.query("SELECT u.FirstName, u.LastName, Status, l.name, access, s.CreatedDate  FROM status s join user u on s.UserId = u.UserId join location l on s.LocationId = l.LocationId where access=0", BeanPropertyRowMapper.newInstance(Status.class));
 	}
-	
-	
-	
-
 }
