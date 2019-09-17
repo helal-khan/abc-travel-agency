3f02c457c6973319a94d9f65e4663d8cb2cb470b
Mohammad Helal Khan <m.helal.k@gmail.com>
2019-09-17 23:30:35 +0600

    Update ProfileDao.java

--- abc-travel-agency/src/com/abctravelagency/dao/ProfileDao.java
+++ abc-travel-agency/src/com/abctravelagency/dao/ProfileDao.java
@@ -1,21 +1,16 @@
 package com.abctravelagency.dao;
 
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 
-import org.hibernate.validator.internal.xml.binding.PayloadType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.abctravelagency.model.Location;
 import com.abctravelagency.model.Status;
@@ -37,53 +32,21 @@ public class ProfileDao {
 	}
 	
 	public List<Location> getLocation() {
-		return jdbc.query("SELECT * FROM location", new RowMapper<Location>() {
-			public Location mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Location location = new Location();
-				location.setLocationId(rs.getInt("LocationId"));
-				location.setName(rs.getString("Name"));
-				location.setActive(rs.getInt("Active"));
-				location.setCreatedDate(rs.getTimestamp("CreatedDate"));
-				location.setUpdatedDate(rs.getTimestamp("UpdatedDate"));
-				return location;
-			}
-		});
+		return jdbc.query("SELECT * FROM location", BeanPropertyRowMapper.newInstance(Location.class));
 	}
 	
 	public List<Status> getStatusByUserId(int userId) {
-		return jdbc.query("SELECT StatusId, Status, l.name, access, s.CreatedDate  FROM status s join location l on s.LocationId = l.LocationId where UserId="+userId, new RowMapper<Status>() {
-			public Status mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Status status = new Status();
-				status.setStatusId(rs.getInt("StatusId"));
-				status.setStatus(rs.getString("Status"));
-				status.setLocation(rs.getString("Name"));
-				status.setAccess(rs.getInt("Access"));
-				status.setCreatedDate(rs.getTimestamp("CreatedDate"));
-				return status;
-			}
-		});
+		return jdbc.query("SELECT StatusId, Status, l.name, access, s.CreatedDate  FROM status s join location l on s.LocationId = l.LocationId where UserId="+userId, BeanPropertyRowMapper.newInstance(Status.class));
 	}
 	
 	public User getUserByUsername(String username) {
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue("username", username);
-		return jdbc.queryForObject("Select * from user where Username = :username", params, new RowMapper<User>() {
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user=new User();
-				user.setUserId(rs.getInt("UserId"));
-				user.setFirstName(rs.getString("FirstName"));
-				user.setLastName(rs.getString("LastName"));
-				user.setEmail(rs.getString("Email"));
-				user.setPhone(rs.getString("Phone"));
-				user.setCreatedDate(rs.getTimestamp("CreatedDate"));
-				return user;
-			}
-		});
+		return jdbc.queryForObject("Select * from user where Username = :username", params, BeanPropertyRowMapper.newInstance(User.class));
 	}
 	
 	public Boolean createPost(Status status) {
 		System.out.println(status);
-		
 		MapSqlParameterSource statusParams = new MapSqlParameterSource();
 		statusParams.addValue("status", status.getStatus());
 		statusParams.addValue("locationId", status.getLocationId());
@@ -106,7 +69,6 @@ public class ProfileDao {
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue("statusId", statusId);
 		params.addValue("access", access);
-
 		return jdbc.update("UPDATE status SET Access=:access WHERE StatusId=:statusId", params) == 1;
 	}
 
